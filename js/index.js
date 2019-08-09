499b00f82ff254c4104e1f8d751131f358aeb84b
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-09 19:20:28 -0400

    added two similar events with stopPropagation working properly for them

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
 const allLinks = document.querySelectorAll('.nav .nav-link');
 const headerText = document.querySelector('.logo-heading');
 const introImg = document.querySelector('.intro-img');
-
+const container = document.querySelector('.home');
+const firstImg = document.querySelector('.img-content');
+const content = document.querySelector('.content-section');
 
 
 
@@ -19,7 +21,27 @@ headerText.addEventListener('mouseover', (e) => {
 
 introImg.addEventListener('dblclick', (e) => {
     introImg.style.display = 'none';
-})
+});
+
+container.addEventListener('click', (event) => {
+    
+    container.style.background = 'salmon';
+    container.stopPropagation();
+});
+
+function bigger(event) {
+    event.stopPropagation();
+    event.currentTarget.style.transform = 'scale(1.1)';
+}
+
+firstImg.addEventListener('click', bigger);
+
+content.addEventListener('click', () => {
+    
+    content.style.background = 'lightblue';
+});
+
+
 
 
 
