d6a7384bf238408753baa0beb4a75776334af5e1
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-08 20:00:38 -0400

    added first event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+const allLinks = document.querySelectorAll('.nav .nav-link');
+console.log(allLinks);
+allLinks.forEach(item => item.addEventListener('click', (e) => {
+    alert('404 page not found');
+}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
