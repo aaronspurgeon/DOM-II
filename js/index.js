3600b0a68484c377ef1ae769b9ebe744d4acf2f3
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-08 20:06:49 -0400

    added second event using mouseover

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
 // Your code goes here
 const allLinks = document.querySelectorAll('.nav .nav-link');
-console.log(allLinks);
+const headerText = document.querySelector('.logo-heading');
+
+
+
+
+
 allLinks.forEach(item => item.addEventListener('click', (e) => {
     alert('404 page not found');
 }));
 
+headerText.addEventListener('mouseover', () => {
+    headerText.style.transform = 'scale(1.1)';
+    setTimeout(() => {
+        headerText.style.transform = '';
+    }, 500)
+}, false);
 
 
 
