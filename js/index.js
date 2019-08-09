392c2eb700e1328c56c10fa2645b4b83d84aa8d8
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-08 20:46:32 -0400

    added another event using dblclick

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 const allLinks = document.querySelectorAll('.nav .nav-link');
 const headerText = document.querySelector('.logo-heading');
-
+const introImg = document.querySelector('.intro-img');
 
 
 
@@ -10,12 +10,18 @@ allLinks.forEach(item => item.addEventListener('click', (e) => {
     alert('404 page not found');
 }));
 
-headerText.addEventListener('mouseover', () => {
+headerText.addEventListener('mouseover', (e) => {
     headerText.style.transform = 'scale(1.1)';
     setTimeout(() => {
         headerText.style.transform = '';
     }, 500)
 }, false);
+
+introImg.addEventListener('dblclick', (e) => {
+    introImg.style.display = 'none';
+})
+
+
 
 
 
