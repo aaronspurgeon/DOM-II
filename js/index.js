3f39a05e6a8abffc96a0e61c0ac7a1357cb31aa2
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-12 13:42:18 -0400

    added mouse handler

--- js/index.js
+++ js/index.js
@@ -10,6 +10,8 @@ const content = document.querySelector('.content-section');
 const inverse = document.querySelector('.inverse-content .img-content');
 const pTag = document.querySelectorAll('p');
 const btn = document.querySelectorAll('.btn');
+let nav = document.querySelector('nav');
+let lastX;
 
 
 
@@ -61,6 +63,14 @@ window.addEventListener('keyup', event => {
     }
 });
 
+nav.addEventListener('mousemove', event => {
+    if (event.button == 0) {
+        lastX = event.clientX;
+        window.addEventListener('mousemove', moved);
+        event.preventDefault();
+    }
+});
+
 
 // functions
 function bigger(event) {
@@ -82,6 +92,17 @@ function displayNone(event) {
     event.stopPropagation();
     event.currentTarget.style.display = 'none';
 };
+
+function moved(event) {
+    if (event.buttons == 0) {
+        window.removeEventListener('mousemove', moved);
+    } else {
+        let dist = event.clientX - lastX;
+        let newWidth = Math.max(10, nav.offsetWidth + dist);
+        nav.style.width = newWidth + 'px';
+        lastX = event.clientX;
+    }
+}
 
 
 // GSAP animations
